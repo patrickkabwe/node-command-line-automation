ef2b7bcdecefc57d04830d26bfb64b425173d9ad
Patrick Kabwe <patrickckabwe@gmail.com>
2022-06-17 17:00:49 +0200

    chore: clean up

--- handlers.js
+++ handlers.js
@@ -84,22 +84,4 @@ const getAllCustomers = async ({ access_token, companyId }) => {
   }
 };
 
-function convertToCSV(objArray) {
-  var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
-  var str = "";
-
-  for (var i = 0; i < array.length; i++) {
-    var line = "";
-    for (var index in array[i]) {
-      if (line != "") line += ",";
-
-      line += array[i][index];
-    }
-
-    str += line + "\r\n";
-  }
-
-  return str;
-}
-
 export { getCatalogItemDetails, getCatalogItems, getAllCustomers };
